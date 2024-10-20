fb4b58aa20aa397c9de6381fddc516fcfe1f7504
Emir Byashimov <97027779+em1racle@users.noreply.github.com>
2024-10-21 00:15:11 +0300

    Add files via upload

---
+++ newsToDay/newsToDay/Application/newsToDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  newsToDayApp.swift
+//  newsToDay
+//
+//  Created by Emir Byashimov on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct newsToDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
