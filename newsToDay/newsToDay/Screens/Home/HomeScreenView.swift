0489103030e8068697d279ed96567970d524cf5a
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-21 21:29:44 +0200

    Created CategoriesView

--- newsToDay/newsToDay/Screens/Home/HomeScreenView.swift
+++ newsToDay/newsToDay/Screens/Home/HomeScreenView.swift
@@ -7,18 +7,36 @@
 
 import SwiftUI
 
+enum Categories: String, CaseIterable {
+    case sports = "Sports"
+    case politics = "Politics"
+    case life = "Life"
+    case gaming = "Gaming"
+    case animals = "Animals"
+    case nature = "Nature"
+    case food = "Food"
+    case art = "Art"
+    case history = "History"
+    case fashion = "Fashion"
+    case covid19 = "Covid-19"
+    case middleEast = "Middle East"
+}
+
 struct HomeScreenView: View {
     @State private var searchText = ""
+    @State private var selectedCategory: Categories = .sports
     
     var body: some View {
         NavigationView {
-            ScrollView() {
+            ScrollView(.vertical) {
                 VStack(alignment: .leading) {
                     Text("Discover things of this world")
                         .foregroundStyle(.secondary)
                         .padding(.bottom, 32)
                     
                     SearchView(searchText: $searchText)
+                    
+                    CategoriesView(selectedCategory: $selectedCategory)
                 }
                 .navigationTitle("Browse")
                 .padding([.horizontal, .bottom])
