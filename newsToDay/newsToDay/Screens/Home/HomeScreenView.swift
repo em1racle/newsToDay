5a556ff7471393428085358a5390acce500ef2bd
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-21 19:32:12 +0200

    Created HomeScreenView with new directory

---
+++ newsToDay/newsToDay/Screens/Home/HomeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreenView.swift
+//  newsToDay
+//
+//  Created by Иван Семикин on 21/10/2024.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreenView()
+}
