54aa849bbdecba3f2a229151ac8b084e68a35464
SergeyZakurakin <pricezak@gmail.com>
2024-10-23 21:22:56 -0400

    create screen

---
+++ newsToDay/newsToDay/NewsDescriptionView/NewsDescriptionView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsDescriptionView.swift
+//  newsToDay
+//
+//  Created by Sergey Zakurakin on 10/23/24.
+//
+
+import SwiftUI
+
+struct NewsDescriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsDescriptionView()
+}
