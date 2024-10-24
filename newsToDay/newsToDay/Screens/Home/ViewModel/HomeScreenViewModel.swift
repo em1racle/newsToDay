abc709b76f67b76452ad13e656cb3eae8e892e2f
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-24 18:14:40 +0200

    Created searchNews(..) in HomeScreenViewModel

--- newsToDay/newsToDay/Screens/Home/ViewModel/HomeScreenViewModel.swift
+++ newsToDay/newsToDay/Screens/Home/ViewModel/HomeScreenViewModel.swift
@@ -29,4 +29,19 @@ final class HomeScreenViewModel: ObservableObject {
             }
         }
     }
+    
+    func searchNews(query: String) {
+        apiClient.searchNews(query: query) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let articles):
+                    self?.articles = articles
+                    self?.errorMessage = nil
+                case .failure(let error):
+                    self?.articles = []
+                    self?.errorMessage = error.localizedDescription
+                }
+            }
+        }
+    }
 }
