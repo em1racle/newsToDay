c20a2cab67ef1464ca1088edf43ef3f926ae8872
SergeyZakurakin <pricezak@gmail.com>
2024-10-24 21:41:34 -0400

    fix design

--- newsToDay/newsToDay/NewsDescriptionView/NewsDescriptionView.swift
+++ newsToDay/newsToDay/NewsDescriptionView/NewsDescriptionView.swift
@@ -8,51 +8,82 @@
 import SwiftUI
 
 struct NewsDescriptionView: View {
+    let article: Article
+    
     var body: some View {
-        NavigationStack{
-        ScrollView {
-            VStack {
-                ZStack {
-                        Image(.testImageOfNews)
-                            .resizable()
+        NavigationView{
+            ScrollView {
+                VStack {
+                    ZStack {
+                        if let urlToImage = article.urlToImage, let url = URL(string: urlToImage) {
+                            AsyncImage(url: url) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(height: 384)
+                                 
+                                 
+                            } placeholder: {
+                                Color.gray
+                            }
                             .frame(height: 384)
-                            .frame(maxWidth: .infinity)
+                            .cornerRadius(10)
+                        } else {
+                            Image(.imageNotFound)
+                                .resizable()
+                                .frame(height: 384)
+                                .cornerRadius(10)
+                        }
                         
                         Text("fdsfsd")
                             .font(.title)
                             .fontWeight(.bold)
                             .foregroundStyle(.white)
                     }
-                    Text("Result")
+                    Spacer()
+                    Text(article.title)
                         .font(.title)
                         .fontWeight(.bold)
+                        .foregroundStyle(.black)
+                        .frame(width: 300)
+                        .cornerRadius(10)
+                        .padding()
                     
-                    Text("fdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasafasdfsafdsaffdfsdfsdfsafsafasaf")
-                        .font(.title) // redo
+                    Text(article.description ?? "No description available.")
+                        .font(.body)
+                        .frame(width: 300)
                 }
-            .toolbar() {
-                ToolbarItem(placement: .topBarLeading) {
-                    NavigationLink(destination: OnboardingView()) {
-                        Image("user1")
-                            .foregroundStyle(.white)
+                
+               
+                .toolbar() {
+                    ToolbarItem(placement: .topBarLeading) {
+                        NavigationLink(destination: OnboardingView()) {
+                            Image("user1")
+                                .foregroundStyle(.white)
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .topBarTrailing) {
+                        NavigationLink(destination: OnboardingView()) {
+                            Image("user1")
+                                .foregroundStyle(.white)
+                        }
                     }
                 }
-                
-                ToolbarItem(placement: .topBarTrailing) {
-                    NavigationLink(destination: OnboardingView()) {
-                        Image("user1")
-                            .foregroundStyle(.white)
-                    }
-                }
-                
-                
-            }
             }
             .ignoresSafeArea()
-  
         }
     }
 }
 #Preview {
-    NewsDescriptionView()
+    NewsDescriptionView(article: Article(
+        source: Source(id: nil, name: "Sample News Source"),
+        author: "John Doe",
+        title: "Sample Title Sample Title Sample Title Sample Title",
+        description: "Sample Title Sample Title Sample Title Sample Title.",
+        url: "https://example.com",
+        urlToImage: "sample-image.jpg",
+        publishedAt: "2024",
+        content: "This is the content of the sample article."
+    ))
 }
