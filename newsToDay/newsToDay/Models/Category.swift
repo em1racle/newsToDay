59cf2e9c3d26932d2102e2f13426792e721dd8d7
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-27 11:15:47 +0100

    Code refactoring in HomeScreenView with all components and views

---
+++ newsToDay/newsToDay/Models/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  newsToDay
+//
+//  Created by Иван Семикин on 27/10/2024.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case sports = "Sports"
+    case business = "Business"
+    case entertainment = "Entertainment"
+    case general = "General"
+    case health = "Health"
+    case science = "Science"
+    case technology = "Technology"
+}
--- newsToDay/newsToDay/Screens/Home/Views/CategoriesView.swift
+++ newsToDay/newsToDay/Screens/Home/Views/CategoriesView.swift
@@ -7,16 +7,6 @@
 
 import SwiftUI
 
-enum Category: String, CaseIterable {
-    case sports = "Sports"
-    case business = "Business"
-    case entertainment = "Entertainment"
-    case general = "General"
-    case health = "Health"
-    case science = "Science"
-    case technology = "Technology"
-}
-
 struct CategoriesView: View {
     @Binding var selectedCategory: Category
     
@@ -24,21 +14,32 @@ struct CategoriesView: View {
         ScrollView(.horizontal) {
             HStack {
                 ForEach(Category.allCases, id: \.self) { category in
-                    Button {
-                        selectedCategory = category
-                    } label: {
-                        let isSelected = selectedCategory == category
-                        
-                        Text(category.rawValue)
-                            .padding(.vertical, 8)
-                            .padding(.horizontal, 16)
-                            .foregroundStyle(isSelected ? .white : .gray)
-                            .background(isSelected ? .blue : Color(.systemGray6))
-                            .clipShape(.rect(cornerRadius: 16))
-                    }
+                    CategoryButton(
+                        category: selectedCategory,
+                        isSelected: selectedCategory == category) {
+                            selectedCategory = category
+                        }
                 }
             }
             .padding()
+        }
+    }
+}
+
+// MARK: - CategoryButton
+struct CategoryButton: View {
+    let category: Category
+    let isSelected: Bool
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(category.rawValue)
+                .padding(.vertical, 8)
+                .padding(.horizontal, 16)
+                .foregroundColor(isSelected ? .white : .gray)
+                .background(isSelected ? Color.blue : Color(.systemGray6))
+                .cornerRadius(16)
         }
     }
 }
